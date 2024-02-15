81f909ff59df69b5a22d8839fe311bd4af8fd57f
Javier Chavez <77762186+jannisce@users.noreply.github.com>
2024-02-14 21:02:23 -0600

    refactor: minor changes

--- frontend/src/components/HeroSection/HeroSection.jsx
+++ frontend/src/components/HeroSection/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div className='bg-white overflow-hidden relative'>
+    <div className='bg-white overflow-hidden relative px-32 py-12'>
       <div className='text-start w-1/2 py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20'>
         <h2 className='text-3xl font-extrabold text-black sm:text-4xl'>
           <span className='block'>Adopta un Amigo para Toda la Vida</span>
