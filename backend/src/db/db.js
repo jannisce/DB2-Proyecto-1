d1613efbc7b0752ccbfda8cdcb19f3436766bca7
Andres Eduardo Quezada Bendaña <andresequez@gmail.com>
2024-02-15 15:29:42 -0600

    feat: fix db name

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,14 +1,18 @@
 import { MongoClient } from 'mongodb'
 
-const MONGODB_URI = 'mongodb+srv://andresquez:12345@andresquez.iiqebxb.mongodb.net/nombre_de_tu_base_de_datos?retryWrites=true&w=majority'
+const MONGODB_URI = 'mongodb+srv://andresquez:12345@andresquez.iiqebxb.mongodb.net/pr1?retryWrites=true&w=majority'
 
 let db
 
 export async function connect() {
   try {
-    const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    const client = new MongoClient(MONGODB_URI)
     await client.connect()
     db = client.db()
+    // print collection names
+    const collections = await db.listCollections().toArray()
+    console.log(collections.map(collection => collection.name))
+
     console.log('Conexión exitosa a MongoDB Atlas')
   } catch (err) {
     console.error('Error al conectar a MongoDB Atlas:', err)
