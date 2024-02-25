4ed4ea7145a5f42f5a18bbfc4b93ba9496b9d718
Javier Chavez <77762186+jannisce@users.noreply.github.com>
2024-02-25 00:03:05 -0600

    style: change input style

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -88,17 +88,24 @@ const RegisterPage = () => {
         <form onSubmit={handleSubmit}>
           {fields.map((field) => (
             <div key={field} className='mb-4'>
-              <label htmlFor={field} className='font-bold mr-2'>
-                {field.charAt(0).toUpperCase() + field.slice(1)}:
+              <label
+                htmlFor={field}
+                className='relative block overflow-hidden border-b border-gray-200 bg-transparent pt-3 focus-within:border-blue-600'
+              >
+                <input
+                  type='text'
+                  id={field}
+                  name={field}
+                  placeholder={formData[field]}
+                  value={formData[field]}
+                  onChange={handleChange}
+                  className='peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm'
+                />
+
+                <span className='absolute start-0 top-2 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs'>
+                  {field.charAt(0).toUpperCase() + field.slice(1)}
+                </span>
               </label>
-              <input
-                type='text'
-                id={field}
-                name={field}
-                value={formData[field]}
-                onChange={handleChange}
-                className='border border-gray-300 rounded-md px-2 py-1'
-              />
             </div>
           ))}
           <button
